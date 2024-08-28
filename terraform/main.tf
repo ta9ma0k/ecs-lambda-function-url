6450e41daccf66567fa69044d3b2469c29bd5325
ta9ma0k <0ismore@gmail.com>
2024-08-28 12:38:54 +0900

    deploy lambda functin

--- terraform/main.tf
+++ terraform/main.tf
@@ -23,6 +23,49 @@ module "iam_policy_copilot_deploy_attachment" {
   copilot_envs     = ["test"]
 }
 
+resource "aws_iam_role" "lambda_exec_role" {
+  name = "lambda-exec-role"
+  assume_role_policy = data.aws_iam_policy_document.lambda_assume_role.json
+}
+
+resource "aws_iam_role_policy_attachment" "lambda" {
+  role = aws_iam_role.lambda_exec_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+data "aws_iam_policy_document" "lambda_assume_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    effect  = "Allow"
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_lambda_function" "my_lambda_function" {
+  function_name = "http-test-function"
+  handler       = "handler.main"
+  runtime       = "python3.12"
+  role          = aws_iam_role.lambda_exec_role.arn
+  filename      = "../functions/lambda.zip"
+}
+
+resource "aws_lambda_function_url" "my_lambda_function_url" {
+  function_name = aws_lambda_function.my_lambda_function.function_name
+  authorization_type = "AWS_IAM"
+}
+
+resource "aws_cloudwatch_log_group" "lambda_log_group" {
+  name = "/aws/lambda/${aws_lambda_function.my_lambda_function.function_name}"
+  retention_in_days = 7
+}
+
+output "function_url" {
+  value = aws_lambda_function_url.my_lambda_function_url.function_url
+}
+
 output "role_name" {
   value = module.iam_role_api_deploy_github_oidc.name 
 }
