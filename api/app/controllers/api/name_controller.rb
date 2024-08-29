c721b63f9812afdd1c9281f61ee9b3b70fe4013c
ta9ma0k <0ismore@gmail.com>
2024-08-29 10:24:30 +0900

    unused function url

--- api/app/controllers/api/name_controller.rb
+++ api/app/controllers/api/name_controller.rb
@@ -1,7 +1,7 @@
-require 'json'
-
 class Api::NameController < ApplicationController
   def index
-    render status: :ok, json: JSON.parse(Invoker.new.call)
+    invoker = Invoker.new
+    lambda_result = invoker.call
+    render status: :ok, json: { lambda_result: }
   end
 end
